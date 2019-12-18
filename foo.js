f367e2a4fc5dbda962a7aabdac7c8e10107e576e
Nao YONASHIRO <owan.orisano@gmail.com>
2019-12-19 02:18:42 +0900

    feat: initial commit

---
+++ foo.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    request: {
+      path: '/foo/:id',
+      method: 'GET',
+      query: {
+        q: '{:someQueryStrings}',
+      },
+      values: {
+        id: 'yosuke',
+        someQueryStrings: 'foo'
+      },
+    },
+    response: {
+      headers: {
+        'x-csrf-token': 'csrf-token', 
+      },
+      body: {
+        message: '{:greeting} {:id} {:someQueryStrings}',
+        images: '{:images}',
+        themes: '{:themes}',
+      },
+      values: {
+        greeting: 'hello',
+        images: [
+          'http://example.com/foo.jpg',
+          'http://example.com/bar.jpg',
+        ],
+        themes: {
+          name: 'green',
+        },
+      }
+    },
+  },
+]
